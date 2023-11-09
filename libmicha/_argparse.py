e9c93e6a8947648f8ead1ff981e26e8aca8852dc
Michaël Paulon <michael@paulon.org>
2023-11-09 16:36:16 +0100

    📝 DOC: Add gist url for StoreDict

--- libmicha/_argparse.py
+++ libmicha/_argparse.py
@@ -2,10 +2,10 @@ import argparse
 from typing import Optional
 
 
-# https://gist.github.com/fralau/061a4f6c13251367ef1d9a9a99fb3e8d
 class StoreDict(argparse.Action):
     """Argparse action used to store arguments in the for key=value in a dict.
     >>> parser.add_argument("key_value", action=store_dict, type=str, nargs="*", default={})
+    Inspired by https://gist.github.com/fralau/061a4f6c13251367ef1d9a9a99fb3e8d
     """
 
     def __call__(
