df180c9442bce6fa9ad15700f2ea7aa3aa010128
Michaël Paulon <michael@paulon.org>
2024-01-31 16:20:43 +0100

    backup

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # LibMicha
 My own python library containing random usefull functions used in a lot of my projects.
 
+## Installation
+LibMicha is available on PyPI. You can install it using:
+```
+pip install libmicha
+```
+
 ## Development
 Setup git repository
 ```
 git config --local include.path .gitconfig 
 ```
 
-# Thanks and Copyrights Information
+## Thanks and Copyrights Information
 #### Resources
  * [Emojis in commits](https://gitmoji.dev/)
 
--- libmicha/_argparse.py
+++ libmicha/_argparse.py
@@ -1,10 +1,33 @@
 import argparse
-from typing import Optional
+from typing import Optional, Tuple
+
+
+class Cli:
+    _cli: dict[Tuple[str, str], str]
+
+    @classmethod
+    @property
+    def cliobjects(cls):
+        return cls._cli
+
+
+def climethod(command, *args):
+    class decorator:  # https://stackoverflow.com/a/54316392
+        def __init__(self, fn):
+            self.fn = fn
+
+        def __set_name__(self, owner, name):
+            if getattr(owner, "_cli", None) is None:
+                setattr(owner, "_cli", {})
+            owner._cli[(command, *args)] = self.fn.__name__
+            setattr(owner, name, self.fn)
+
+    return decorator
 
 
 class StoreDict(argparse.Action):
     """Argparse action used to store arguments in the for key=value in a dict.
-    >>> parser.add_argument("key_value", action=store_dict, type=str, nargs="*", default={})
+    >>> parser.add_argument("key_value", action=libmicha.StoreDict, type=str, nargs="*", default={})
     Inspired by https://gist.github.com/fralau/061a4f6c13251367ef1d9a9a99fb3e8d
     """
 
